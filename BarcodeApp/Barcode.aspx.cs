e1839b379533e1d3145aa8b785e3fddc57f85498
Jeff Bolt <fryguy0033@gmail.com>
2022-04-11 08:09:55 -0500

    Add project files.

---
+++ BarcodeApp/Barcode.aspx.cs
@@ -0,0 +1,111 @@
+﻿using Aspose.BarCode.BarCodeRecognition;
+using Aspose.BarCode.Generation;
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web.UI;
+
+namespace BarcodeApp
+{
+    public partial class Barcode : Page
+    {
+        public string BaseDirectory
+        {
+            get
+            {
+                return Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase).Replace(@"file:\", "");
+            }
+        }
+
+        public string BarcodeDirectory
+        {
+            get
+            {
+                return Path.Combine(Directory.GetParent(BaseDirectory).Parent.FullName, "Barcodes");
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                var dataSource = EncodeTypes.AllEncodeTypes.ToList().OrderBy(x => x.TypeName);
+                ddlEncodeType.DataSource = dataSource;
+                ddlEncodeType.DataTextField = "TypeName";
+                ddlEncodeType.DataValueField = "TypeIndex";
+                ddlEncodeType.DataBind();
+                ddlEncodeType.SelectedIndex = EncodeTypes.Code128.TypeIndex;
+
+                txtValue.MaxLength = 128;  // TODO: determine max len
+                txtValue.Text = Guid.NewGuid().ToString();
+                //string dataDir = Path.Combine(Directory.GetParent(BaseDirectory).Parent.FullName, "Barcodes");
+                //if (!Directory.Exists(BarcodeDirectory)) Directory.CreateDirectory(BarcodeDirectory);
+                //string codeText = "1234567";
+                //string filePath = Path.Combine(BarcodeDirectory, "Output.jpg");
+                //if (File.Exists(filePath)) File.Delete(filePath);
+                //SaveAsImage(filePath, codeText, EncodeTypes.Code128, BarCodeImageFormat.Jpeg);
+
+                //string demoFile = Path.Combine(BarcodeDirectory, "GetStarted.png"); //@"d:\template.jpg";
+                //ReadFromImage(demoFile);
+            }
+        }
+
+        private Bitmap GenerateBitmap(string codeText, BaseEncodeType encodeType)
+        {
+            // https://products.aspose.com/barcode/net/
+            // Instantiate object and set differnt barcode properties
+            BarcodeGenerator generator = new BarcodeGenerator(encodeType, codeText);
+            generator.Parameters.Barcode.XDimension.Millimeters = 1f;
+            
+            // Save the image to your system and set its image format to Jpeg
+            return generator.GenerateBarCodeImage();
+        }
+
+        private void SaveAsImage(string filePath, string codeText, BaseEncodeType encodeType, BarCodeImageFormat imageFormat)
+        {
+            // https://products.aspose.com/barcode/net/
+            // Instantiate object and set differnt barcode properties
+            BarcodeGenerator generator = new BarcodeGenerator(encodeType, codeText);
+
+            generator.Parameters.Barcode.XDimension.Millimeters = 1f;
+
+            // Save the image to your system and set its image format to Jpeg
+            generator.Save(filePath, imageFormat);
+        }
+
+        private void ReadFromImage(string filePath)
+        {
+            using (BarCodeReader reader = new BarCodeReader(filePath))
+            {
+                foreach (BarCodeResult result in reader.ReadBarCodes())
+                {
+                    Console.WriteLine("Type: " + result.CodeType);
+                    Console.WriteLine("CodeText: " + result.CodeText);
+                }
+            }
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = GenerateBitmap(txtValue.Text, BaseEncodeType.Parse(ddlEncodeType.SelectedItem?.Text));
+            if (bmp != null)
+            {
+                pnlResult.Visible = true;
+
+                using (var ms = new MemoryStream())
+                {
+                    bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    string base64 = Convert.ToBase64String(ms.GetBuffer());
+                    string dataUri = $"data:image/png;base64,{base64}";
+
+                    //imgResult.Height = bmp.Height;
+                    //imgResult.Width = bmp.Width;
+                    imgResult.ImageUrl = dataUri;
+                }
+            }
+        }
+    }
+}
---
+++ BarcodeApp/Default.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading;
+using System.Web.UI;
+
+namespace BarcodeApp
+{
+	public partial class _Default : Page
+	{
+		//private static Random random = new Random();
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Page.Title = $"Home - {Properties.Settings.Default.AppName}";
+
+			// https://security-code-scan.github.io/#SCS0005
+			var sb = new StringBuilder();
+			for (int i = 1; i <= 10; i++)
+			{
+				Thread.Sleep(1);
+				string weak = GenerateWeakRandomNumber(1234);
+				string strong = GenerateStrongRandomNumber();
+				sb.Append($"Weak: {weak}    Strong: {strong}    <br \\>");
+			}
+
+			lblContent.Text = sb.ToString();
+			lblContent.Text += "<br />" + GetHtmlEncodedValue("<");
+			lblContent.Text += "<br />" + GetHtmlEncodedValue("⇑");
+			lblContent.Text += "<br />" + GetHtmlEncodedValue("❌");
+		}
+
+		private static string GenerateWeakRandomNumber(int? seed = null)
+		{
+			var rnd = seed == null ? new Random() : new Random((int)seed);
+			byte[] buffer = new byte[16];
+			rnd.NextBytes(buffer);
+			return BitConverter.ToString(buffer, 0);
+		}
+
+		private static string GenerateStrongRandomNumber()
+		{
+			byte[] buffer = new byte[16];
+			var rnd = RandomNumberGenerator.Create();
+			rnd.GetBytes(buffer);
+			return BitConverter.ToString(buffer, 0);
+		}
+
+		private static string GetHtmlEncodedValue(string value)
+		{
+			var encoder = HtmlEncoder.Create();
+			string encoded = encoder.Encode(value);
+			if (encoded.StartsWith("&#x"))
+			{
+				Console.WriteLine("HEX format detected, converting to DEC format...");
+				// Ex: "❌" => "&#x274C;" => "247C" => 10060 => "&#10060;"
+				////string hex = encoded.Remove(0, 3).TrimEnd(';');
+				////int dec = int.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+				////return string.Format("&#{0}", dec);
+
+				string hex = encoded.Remove(0, 3).TrimEnd(';');
+				var ca = encoded.ToCharArray().ToList().RemoveAll(x => !char.IsLetterOrDigit(x));
+
+				if (int.TryParse(hex, NumberStyles.HexNumber, CultureInfo.CurrentCulture, out int dec))
+					return string.Format("&#{0}", dec);
+				else
+					return encoded;
+			}
+			else
+			{
+				return encoded;
+			}
+			//string hexValue = intValue.ToString("X");
+			//int.Parse(HtmlEncoder.Create().Encode("❌").Remove(0, 3).TrimEnd(';'), System.Globalization.NumberStyles.HexNumber)
+		}
+	}
+}
